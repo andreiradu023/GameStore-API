0bc0fc8ef23ab3ceab2a97a2fb375299f20169a7
Andrei Radu <andrei.agg95@yahoo.com>
2023-09-15 15:27:04 +0300

    Initial commit

---
+++ src/main/java/com/andreiradu/gamestore/api/GamestoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.andreiradu.gamestore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamestoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andreiradu/gamestore/api/GamestoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreiradu.gamestore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
