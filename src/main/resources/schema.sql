74b86981e20e17114729eff4b579e63b7aa8974a
Andrei Radu <andrei.agg95@yahoo.com>
2023-09-22 15:23:18 +0300

    Add schema and data for database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE `game_store`;
+
+USE `game_store`;
+
+CREATE TABLE `users`(
+                      `id` BIGINT NOT NULL AUTO_INCREMENT,
+                      `first_name` VARCHAR(40) NOT NULL,
+                      `last_name` VARCHAR(40) NOT NULL,
+                      `email` VARCHAR(100) NOT NULL,
+                      `phone` VARCHAR(15) NOT NULL,
+                      `address` VARCHAR(200) NOT NULL,
+                       PRIMARY KEY (`id`));
+
+CREATE TABLE `games` (
+                         `id` BIGINT NOT NULL AUTO_INCREMENT,
+                         `title` VARCHAR(100) NOT NULL,
+                         `platform` VARCHAR(200) NOT NULL,
+                         `genre` VARCHAR(200) NOT NULL,
+                         `description` VARCHAR(4000) NOT NULL,
+                         `price` FLOAT NOT NULL,
+                         `image` VARCHAR(200),
+                         `release_date` DATE,
+                         `stock_quantity` INT,
+                          PRIMARY KEY (`id`));
+
+CREATE TABLE `orders` (
+                        `id` BIGINT NOT NULL AUTO_INCREMENT,
+                        `user_id` BIGINT,
+                        `order_date` DATE,
+                        `total_price` FLOAT NOT NULL,
+                        PRIMARY KEY (`id`),
+                        FOREIGN KEY (user_id) REFERENCES users(id));
+
+CREATE TABLE `order_items` (
+                             `id` BIGINT NOT NULL AUTO_INCREMENT,
+                             `order_id` BIGINT,
+                             `game_id` BIGINT,
+                             `quantity` INT,
+                             `unit_price` FLOAT,
+                             `subtotal` FLOAT AS (unit_price * quantity),
+                             PRIMARY KEY (`id`),
+                             FOREIGN KEY (order_id) REFERENCES orders(id),
+                             FOREIGN KEY (game_id) REFERENCES games(id));
+
+CREATE TABLE `cart_items` (
+                              `id` BIGINT NOT NULL AUTO_INCREMENT,
+                              `user_id` BIGINT,
+                              `game_id` BIGINT,
+                              `quantity` INT,
+                              PRIMARY KEY (id),
+                              FOREIGN KEY (user_id) REFERENCES users(id),
+                              FOREIGN KEY (game_id) REFERENCES games(id));
